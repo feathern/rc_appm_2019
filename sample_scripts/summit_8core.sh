3f3bccfb172fa43120be01a1f3e1d0e7bc3b0c5e
Nick Featherstone <feathern@gmail.com>
2019-08-22 13:08:13 -0600

    Added Summit scripts # Please enter the commit message for your changes. Lines starting

---
+++ sample_scripts/summit_8core.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+#SBATCH --nodes=1
+#SBATCH --time=00:10:00
+#SBATCH --qos=testing
+#SBATCH --partition=shas
+#SBATCH --ntasks=8
+#SBATCH --job-name=mpi-hello2
+#SBATCH --output=mpi-hello2.%j.out
+
+
+module purge
+module load intel
+module load impi
+
+cd ../sample_code # the assumption
+
+#The SLURM_NTASKS environment variable uses all ntasks specified (in this case, 8)
+mpiexec -np $SLURM_NTASKS ./hello.out
